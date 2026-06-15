73c1f5245f01fef5a2a9200def8892dfcbea1a58
Olive <olivenicholson@shgcham.school.nz>
2026-06-15 12:36:14 +1200

    lesson 7

--- 06_join_the_party/scripts.js
+++ 06_join_the_party/scripts.js
@@ -2,9 +2,11 @@ let nameInput = document.getElementById("nameInput");
 let greetButton = document.getElementById("greetButton");
 let output = document.getElementById("output");
 
-function sayHello() {
-  let name = nameInput.value;
-  output.innerText = "Welcome, " + name;
+function sayHello() { //message opperation//
+  let name = nameInput.value; //gets name value, stores it//
+  let role = roleInput.value;
+  let party_name = party_nameInput.value;
+  output.innerText = "Welcome, " + name +  " the " + role + " to the " + party_name + "!!!";  //prints welcome (changeable) text + name value//
 }
 
-greetButton.onclick = sayHello;
+greetButton.onclick = sayHello; 
