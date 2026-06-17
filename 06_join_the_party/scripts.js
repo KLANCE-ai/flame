3276be4e0c824ceebef8ecdf2aadb643dcb5f472
Olive <olivenicholson@shgcham.school.nz>
2026-06-17 14:57:48 +1200

    lesson 8

--- 06_join_the_party/scripts.js
+++ 06_join_the_party/scripts.js
@@ -3,10 +3,9 @@ let greetButton = document.getElementById("greetButton");
 let output = document.getElementById("output");
 
 function sayHello() { //message opperation//
-  let name = nameInput.value; //gets name value, stores it//
-  let role = roleInput.value;
-  let party_name = party_nameInput.value;
-  output.innerText = "Welcome, " + name +  " the " + role + " to the " + party_name + "!!!";  //prints welcome (changeable) text + name value//
+  let name = ratingInput.value; //gets name value, stores it//
+
+  output.innerText = "you rated" + rating + " thats pretty cool broo";  //prints welcome (changeable) text + name value//
 }
 
 greetButton.onclick = sayHello; 
--- 07_party_sign_up_form/scripts.js
+++ 07_party_sign_up_form/scripts.js
@@ -1,9 +1,20 @@
 function showSignupMessage() {
   let name = document.getElementById("nameInput").value;
   let region = document.getElementById("regionInput").value;
+  let role = document.getElementById("roleInput").value;
+  let Party_Name = document.getElementById("Party_NameInput").value;
+  let message
+  if (name == ''){
+    messagge = 'brotaco klance needs NAMES'
+  } else if (reigon == ''){
+    message = 'klance died for.. nothing?????'
+  } else {
+    message = `Welcome ${name} from ${region}. \n Your role is ${role} in ${Party_Name}.`
+  }
 
-  let message = `Welcome, ${name} from ${region} Thanks for joining the party`
+  let message = `Welcome ${name} from ${region}. \n Your role is ${role} in ${Party_Name}.`
   document.getElementById("output").innerText = message;
 }
+
 
 document.getElementById("signupButton").onclick = showSignupMessage
