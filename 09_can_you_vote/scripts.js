ce84291e4eb5dbb876a025e78955285f4d00f5da
Olive <olivenicholson@shgcham.school.nz>
2026-06-23 10:01:37 +1200

    lesson 10

--- 09_can_you_vote/scripts.js
+++ 09_can_you_vote/scripts.js
@@ -1,17 +1,25 @@
 // Instructions in the CSS
 
-fuction checkAge = 
-  let age = document.getElement("ageBox").value;
+function checkAge(){
+  let age = document.getElementById("ageBox").value;
+  let message = "";
 
-  when age >= 18 {
+  if (age == '') {
+    message = "you must enter an age";
+  } else if (age > 100) {
+    message = "youre living legend";
+  } else if (age >= 18) {
     message = "You can vote!";
-  } else if age >= 16 {
+  } else if (age >= 16) {
     message = "You can pre-enrol.";
+  } else if (age == 0) {
+    message = "bro still in the womb";
   } else {
-    mesage = "Sorry, you can't register yet.";
+    message = "Sorry, you can't register yet.";
   }
 
-  doccument.getelementbyid("#results").internalhtml = message;
+  document.getElementById("results").innerText = message;
+}
 
 
-document.getElementID("submit").onClick = checkAge
+document.getElementById("submit").onclick = checkAge
