bf1d05a9d3001d454a77f989d4a619aa4b02adc5
I.HATE.GIT-HUB <olivenicholson@shgcham.school.nz>
2026-06-29 00:47:20 +0000

    lesson 12

--- 11_party_policies/scripts.js
+++ 11_party_policies/scripts.js
@@ -3,15 +3,23 @@ let policies = [
   "Guarantee spell-forging apprenticeships",
   "Forge-safe housing in high-temperature zones",
   "Universal access to enchanted tools",
-  "Protect natural mana flows from over-mining"
+  "Protect natural mana flows from over-mining",
+  "FORM VOLTRON 24/7"
 ];
 
 function showPolicies() {
   let list = document.getElementById("policyList");
 
-  for (let i = 0; i < policies.length; i++) {
+  for (let policy of policies) {
     let item = document.createElement("li");
-    item.innerText = policies[i];
+    item.innerText = policy;
     list.appendChild(item);
   }
 }
+function addPolicy(){
+  let policy = document.getElementById('textInput').value
+  policies.push(policy)
+}
+
+document.getElementById("showPolicies").onclick = showPolicies;
+document.getElementById("addPolicyButton").onclick = addPolicy;
